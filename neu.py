57098e26b8fa6f9b9c1c01716006e67db2b86924
Anton Bulat <ukr.antoschka@gmx.de>
2017-11-13 15:34:04 +0100

    Meine VERBESSERUNGEN

--- neu.py
+++ neu.py
@@ -22,62 +22,97 @@ class MyAgent(object):
         self.actionset = actionset
 
     def pickRandomAction(self):
-        return self.actionset[num.randint(0, 5)]
+        #actions -> "up": K_w, "left": K_a, "right": K_d, "down": K_s, "none": -
+        return self.actionset[num.randint(0, 4)]
+
+    def pickBestAction(self, state, dict):
+        max = 0
+        counter = 0
+        max_c = 0
+        for v in dict[state]:
+            if (max > v):
+                max = v
+                max_c = counter
+            counter += 1
+        return dict[state].__getitem__(max_c)
+
+    def pickBestActionRndomized(self, state, dict):
+        actionsValues = dict[state]
+        maxQ = max(actionsValues)
+        count = actionsValues.count(maxQ)
+        if count > 1:
+            best = [i for i in range(len(actionsValues)) if actionsValues[i] == maxQ]
+            i = num.random.rand(best)
+            #random.choice(best)
+        else:
+            i = actionsValues.index(maxQ)
+
+        return dict[state].__getitem__(i)
+
+    def foodIsNearer(self, state1, state2):
+        if (abs(state1["snake_head_x"]-state1["food_x"])+ abs(state1["snake_head_y"]-state1["food_y"])
+            >abs(state2["snake_head_x"]-state2["food_x"])+ abs(state2["snake_head_y"]-state2["food_y"])):
+            return True
+        else:
+            return False
+
+    def getSmartReward(self, p, snake, current_state, next_state):
+        if p.game_over:
+            return snake.rewards["loss"]
+        elif self.foodIsNearer(current_state, next_state):
+            return snake.rewards["positive"]
+        elif (self.foodIsNearer(current_state, next_state)):
+            return snake.rewards["negative"]
+        else:
+            return snake.rewards["win"]
 
     # Q-Leaning Algorithm
     # returns a q-dictionary
-    def train(self, p):
+    def train(self, p, snake):
         p.init()
-        nb_frames = 1000
-        # because of the number of states we work with a dictionary with (state,action) as key
+        nb_frames = 100
+        # because of the number of states we work with a dictionary with (state) as key
         # so we always have to check if the key is already in the dictionary
-        # - if not we have to initialize the key with 0
-        q_dic = {}
+        # if not we have to initialize the key with Zeros
+
+        q_dic = {snake.getGameState().values():[0, 0, 0, 0, 0]}
         gamma = 0.75
-        current_state = my_hash(p.getScreenRGB().tolist())  # make the screen hashable
+        current_state = snake.getGameState().values() #my_hash(p.getScreenRGB().tolist())  # make the screen hashable
+        smartReward = snake.rewards["tick"]
         for f in range(nb_frames):
             if p.game_over():
                 p.reset_game()
+                # Bellman equation
+                #q_dic[current_state] = reward + gamma * q_dic[next_state].__getitem__(self.pickBestAction(next_state, q_dic))
+                q_dic[current_state] = smartReward + gamma * q_dic[next_state].__getitem__(self.pickBestAction(next_state, q_dic))
+                smartReward = snake.rewards["tick"]
             # //////////////////////////////////////////////////////////////////////////////////////////
             else:
-                action = self.pickRandomAction()  # select a random action --- good solution?
-                reward = p.act(action)  # get reward
-                next_state = my_hash(p.getScreenRGB().tolist())  # get next state
-                if (current_state, action) not in q_dic:  # if current state is not in the dictionary
-                    q_dic[(current_state, action)] = 0.0  # add initial value
-
-                # find action with maximal q value:
-
-                max_action = self.pickRandomAction()  # first take a random action
-                max = 0
-                if (next_state, max_action) in q_dic:
-                    max = q_dic[(next_state, max_action)]
+                if (num.randint(1, 10) % 10 == 0): # a little bit random inside would be better
+                    action = self.pickRandomAction() # select a random action
                 else:
-                    q_dic[(next_state, max_action)] = 0.0
-                # look if the is an other action in actionset which is better (based on q table)
-                for a in self.actionset:
-                    if (next_state, a) in q_dic:
-                        tmp = q_dic[(next_state, a)]
-                        if tmp > max:
-                            max = tmp
-                            max_action = a
-                    else:
-                        q_dic[(next_state, a)] = 0.0
-                # TODO das hier konvergiert noch nicht sondern wird einfach höher gesetzt...nicht so sinnvoll...
-                q_dic[(current_state, action)] += (reward + gamma * q_dic[(next_state, max_action)])  # Bellman equation
-                print("state, action, maxq",current_state,action,reward + gamma * q_dic[(next_state, max_action)])
+                    action = self.pickBestActionRndomized(current_state, q_dic)  # select random max action value
+                reward = p.act(action)  # get reward
+                next_state = snake.getGameState().values()  # get next state
+                if (next_state) not in q_dic:  # if next state is not in the dictionary
+                    q_dic[(next_state)] = [0, 0, 0, 0, 0]  # add initial value
+                smartReward = self.getSmartReward(p, snake, current_state, next_state)  # get smart reward (Manhatten)
+                #  Bellman equation
+                #q_dic[current_state] = (reward + gamma * q_dic[next_state].__getitem__(self.pickBestAction(next_state, q_dic)))
+                q_dic[current_state] = (smartReward + gamma * q_dic[next_state].__getitem__(self.pickBestAction(next_state, q_dic)))
+                print("state, action, maxq",current_state,action,reward + gamma * q_dic[next_state])
                 current_state = next_state
         return q_dic
 
 
-snake = game.Snake(width=150, height=150, init_length=3)
+snake = game.Snake(width=250, height=250, init_length=3)
 
 p = PLE(snake, fps=30, display_screen=True)
 
 myAgent = MyAgent(p.getActionSet())
 
 start_time = time.time()
-my_q_dic = myAgent.train(p)
+my_q_dic = myAgent.train(p, snake)
 end_time = time.time()
 
 print("TIME OF Q_LEARNING", end_time - start_time)
@@ -92,7 +127,8 @@ print("PROZESS MEMORY INFO: ", process.memory_info().rss)
 #print("SIZE OF DICTIONARY: ", size)
 
 # save the q-dic in a file
-afile = open(r'C:\Users\Julia\Documents\WS17-18\MLPraktikum\q_dic.pkl', 'wb')
-pic.dump(my_q_dic, afile)
+afile = open(os.getcwd() + '\q_dic.pkl', 'wb')
+print(os.getcwd() + '\q_dic.pkl')
+pic.dump(my_q_dic, afile, pic.HIGHEST_PROTOCOL)
 afile.close()
 print("Q_DIC: ",my_q_dic)
